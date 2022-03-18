11b24ac127a8bf483ca3c8d2b93cf011363f9cdc
theludovyc <theludovyc@gmail.com>
2022-03-18 19:10:43 +0100

    Update

--- Script/Grappin.gd
+++ Script/Grappin.gd
@@ -5,29 +5,42 @@ onready var sprite := $Sprite
 onready var line := $Line2D
 onready var parent := get_parent()
 
+var hook_origin:Vector2
+
+var hooked := false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	set_process(false)
 	raycast.add_exception(parent)
 	pass # Replace with function body.
 
 func hook_enabled(b:bool):
 	sprite.visible = b
 
-func launch():
+func launch() -> bool:
 	raycast.enabled = true
 	
 	raycast.force_raycast_update()
 	
-	if raycast.is_colliding():
-		var local_point = parent.to_local(raycast.get_collision_point())
+	var b = raycast.is_colliding()
+	
+	if b:
+		hook_origin = raycast.get_collision_point()
 		
-		line.set_point_position(1, local_point)
 		line.visible = true
 		
-		sprite.position = local_point
+		set_process(true)
+	else:
+		line.visible = false
+		
+		set_process(false)
 		
 	raycast.enabled = false
-		
+	
+	return b
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func _process(delta: float) -> void:
+	sprite.position = parent.to_local(hook_origin)
+	line.set_point_position(1, sprite.position)
--- Script/Leaf.gd
+++ Script/Leaf.gd
@@ -2,24 +2,58 @@ extends State
 
 const Move_Speed = 0.5
 const Stop_Speed = 0.3
+const Rot_Speed = 1
+
+enum State{normal = 0, hook}
+
+var state:int
 
 onready var hook = parent.get_node("Hook")
 
 func state_enabled(b:bool):
 	.state_enabled(b)
 	hook.hook_enabled(b)
+	state = State.normal
 
 func _physics_process(delta: float) -> void:
-	if parent.is_on_floor():
-		parent.speed.y = parent.Grav
-	else:
-		parent.speed.y += parent.Grav
+	match(state):
+		State.normal:
+			if parent.is_on_floor():
+				parent.speed.y = parent.Grav
+			else:
+				parent.speed.y += parent.Grav
 
-	parent.move_and_slide(parent.speed * parent.MulSpeed + Vector2(0, parent.val), Vector2.UP)
+			parent.move_and_slide(parent.speed * parent.MulSpeed + Vector2(0, parent.val), Vector2.UP)
+			
+		State.hook:
+#			parent.global_position = hook.hook_origin + (parent.global_position - hook.hook_origin).rotated(-Rot_Speed * parent.speed.x)
+			
+			var vert = parent.global_position - hook.hook_origin
+			
+			var vert_rot = vert.rotated(parent.speed.x * 2)
+			
+			var bleu = vert_rot - vert
+			
+			parent.position += bleu * delta
+			
+#			var bleu = hook.hook_origin + (parent.global_position - hook.hook_origin).rotated(0) - parent.global_position
+#
+#			var jaune = Vector2(0, -parent.Grav)
+#
+#			var vert = (-bleu).project(jaune)
+#
+#			prints("Leaf", vert)
+#
+#			parent.move_and_slide(bleu, Vector2.UP)
+			pass
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("up"):
-		hook.launch()
+		match(state):
+			State.normal:
+				if hook.launch():
+					parent.speed.y = 0
+					state = State.hook
 
 	if Input.is_action_just_pressed("test_normal"):
 		parent.anim_playback.travel("to_normal")
