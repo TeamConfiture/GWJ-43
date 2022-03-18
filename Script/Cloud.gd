c59dd8080777edfad1791f6fe0f20e097adaec54
Saltyn <cedric.arrabie@univ-pau.fr>
2022-03-18 23:49:30 +0100

    Cloud Rain + Parralax cave  on the way

--- Script/Cloud.gd
+++ Script/Cloud.gd
@@ -6,14 +6,10 @@ export(float) var Cloud_Speed = -15
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta)->void:
 	self.motion_offset.x += Cloud_Speed * delta
 
-
-func _on_Area2D_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.is_doubleclick() :
-		print("brouaaaaaaaaaaaaa")
---
+++ Script/Rain.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+onready var Rain = $Sprite_1/Rain
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+		Rain.visible = false
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.is_doubleclick() :
+		Rain.visible = true
