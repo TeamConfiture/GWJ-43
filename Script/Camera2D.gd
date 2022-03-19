1400e521fb4111edd55a6e33b7b432be9f4d6aec
Saltyn <cedric.arrabie@univ-pau.fr>
2022-03-19 12:19:43 +0100

    Camera smooth

---
+++ Script/Camera2D.gd
@@ -0,0 +1,17 @@
+extends Camera2D
+
+
+
+const camera_limit_right_default = 100 
+const camera_limit_down_default = 100 
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
