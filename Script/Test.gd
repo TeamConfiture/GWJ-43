d5c61a656ef8d83adc8aab0acdcc42b994c50842
Saltyn <cedric.arrabie@univ-pau.fr>
2022-03-19 13:18:12 +0100

    Camera limit, refont limit plateform by tile

--- Script/Test.gd
+++ Script/Test.gd
@@ -4,6 +4,10 @@ onready var hud := $HUD
 
 var coins := 0
 
+
+func _ready():
+	set_camera_limits()
+	
 func _on_Slime_clover_eaten(clovers: PoolStringArray) -> void:
 	hud.update_clovers(clovers)
 
@@ -12,3 +16,13 @@ func _on_Slime_coin_caught() -> void:
 	hud.update_coin(coins)
 	
 	pass # Replace with function body.
+
+func set_camera_limits():
+	var map_limits = $lvl/TileMap_limit.get_used_rect()
+	var map_cellsize = $lvl/TileMap_limit.cell_size
+	
+#	$Slime/Camera2D.limit_top = map_limits.position.y * map_cellsize.y
+#	$Slime/Camera2D.limit_left = map_limits.position.x * map_cellsize.x
+	
+	$Slime/Camera2D.limit_right = map_limits.end.x * map_cellsize.x - 64
+	$Slime/Camera2D.limit_bottom = map_limits.end.y * map_cellsize.y - 64
---
+++ Script/TileMap_limit.gd
@@ -0,0 +1,9 @@
+extends TileMap
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.visible = false
+
