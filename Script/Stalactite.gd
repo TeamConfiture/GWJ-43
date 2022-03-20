a4bc5381fb3df04887fb6d14a2ba061b7d456fb6
Saltyn <cedric.arrabie@univ-pau.fr>
2022-03-20 01:23:27 +0100

    Lvl tuto + stalactite

---
+++ Script/Stalactite.gd
@@ -0,0 +1,14 @@
+extends StaticBody2D
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
+func _on_Area2D_body_entered(body):
+	print(body.state)
+#	if body.state == 3:
+#			body.state = 0
