72305c5c8ebbf5932e21a4ea8b88d9d59425edc3
Saltyn <cedric.arrabie@univ-pau.fr>
2022-04-10 00:18:08 +0200

    Update scene lvl

--- Script/Audio_Amb.gd
+++ Script/Audio_Amb.gd
@@ -1,13 +1,14 @@
 extends Node2D
 
 func _on_PlayerDetectorAMBForest_body_entered(body):
-	BackgroundMusic.play_amb_forest()
+	if body.name == "Slime" :
+		BackgroundMusic.play_amb_forest()
 
 func _on_PlayerDetectorAMBForest_body_exited(body):
 	pass
 
 func _on_PlayerDetectorAMBCavern_body_entered(body):
-	BackgroundMusic.play_amb_cavern()
+	if body.name == "Slime" :BackgroundMusic.play_amb_cavern()
 
 func _on_PlayerDetectorAMBCavern_body_exited(body):
 	pass
