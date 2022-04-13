2fcb8931c25757362be32f365b87f7b04f54b8a6
Saltyn <cedric.arrabie@univ-pau.fr>
2022-04-13 21:50:45 +0200

    Find the way to the door

--- Script/Door.gd
+++ Script/Door.gd
@@ -1,7 +1,9 @@
 extends StaticBody2D
 
+signal door_to_open
 onready var anim_player = $AnimationPlayer
 
 func open():
-#	$root/Game.
+	get_tree().get_root().get_node("Game").find_the_wayout(position)
+
 	anim_player.play("porte")
