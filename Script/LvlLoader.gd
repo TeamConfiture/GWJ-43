16dec86c831b9ae3c8bac5f7511a47bc0e7121d4
Saltyn <cedric.arrabie@univ-pau.fr>
2022-04-13 23:07:28 +0200

    Fix door /button cinematique

--- Script/LvlLoader.gd
+++ Script/LvlLoader.gd
@@ -5,16 +5,17 @@ onready var fader = $AnimationPlayer
 
 
 func _ready():
-	pass
-	
-	
-
-func change_lvl_out():
-	$Layer/TransitionColor.visible=true
-	fader.play("transition_out")
-	yield(fader,"animation_finished")
-
-func change_lvl_in():
 	$Layer/TransitionColor.visible=true
 	fader.play("transition_in")
-	yield(fader,"animation_finished")
+	
+
+	
+func change_lvl():
+#	fader.play("transition_out")
+#
+#	yield(fader,"animation_finished")
+
+	fader.play("transition_in")	
+
+
+
