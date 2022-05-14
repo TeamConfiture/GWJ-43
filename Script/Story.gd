f1505b77fea10bf6deb55e212d5a0524985ef07a
Saltyn <cedric.arrabie@univ-pau.fr>
2022-05-14 19:32:48 +0200

    Update Story.gd

--- Script/Story.gd
+++ Script/Story.gd
@@ -19,6 +19,8 @@ func _on_NextBtn_pressed():
 		AudioManager.play_button_next()
 		if page >= 5:
 			$NextBtn.visible=false
+			$Space.visible=false
+			$Arrow.visible=false
 			SceneLoader.change_scene("Game")
 		else:
 			node_var = get_node_or_null('P'+ str(page))
