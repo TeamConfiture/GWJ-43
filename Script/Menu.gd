241466082b22bd19f3b37279d6930c406257d85b
Saltyn <cedric.arrabie@univ-pau.fr>
2022-05-15 00:47:38 +0200

    Fix start with gamepad

--- Script/Menu.gd
+++ Script/Menu.gd
@@ -24,4 +24,4 @@ func _process(_delta: float) -> void:
 
 	if Input.is_action_just_pressed("spit"):
 		AudioManager.play_button_normal()
-		SceneLoader.change_scene("Menu")
+		SceneLoader.change_scene("Game")
